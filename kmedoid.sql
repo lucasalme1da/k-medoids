e0a89be177a57cbd8706819b7315c36f71916dc2
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-05 20:07:50 -0300

    Normalização

--- kmedoid.sql
+++ kmedoid.sql
@@ -707,6 +707,26 @@ create or replace function algoritmoMedoid() returns text as $$
 	end
 $$ language plpgsql;
 
+create or replace function normalizar() returns void as $$
+	declare 
+		xMin dados%ROWTYPE;
+		yMin dados%ROWTYPE;
+		wMin dados%ROWTYPE;
+		zMin dados%ROWTYPE;
+
+		xMax dados%ROWTYPE;
+		yMax dados%ROWTYPE;
+		wMax dados%ROWTYPE;
+		zMax dados%ROWTYPE;
+	
+	begin
+		select max(dados_eixo_x) into xMax from dados;
+		raise notice 'maximo x -> %',xMax;
+
+
+	end
+$$ language plpgsql;
+
 SELECT algoritmoMedoid();
 
 SELECT dados.grupo_id,
